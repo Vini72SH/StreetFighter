98fada7dc0ea9185ccc6eb259ac5f458046e4d6e
Vinícius Santos <120831194+Vini72SH@users.noreply.github.com>
2024-07-18 18:44:37 -0300

    Ken Sprite Bug Fix

--- character.c
+++ character.c
@@ -179,10 +179,9 @@ void characterMove(character *chara, float steps, ushort trajectory, ushort max_
     }
 
     if (trajectory == UP) {
-        if ((chara->y - steps * chara->air_speed * JUMP_CONST) - chara->hurtbox->height/2 >= 0) {
-            chara->y = chara->y - steps * chara->air_speed * JUMP_CONST;
-            chara->hurtbox->y = chara->y;
-        }
+        chara->y = chara->y - steps * chara->air_speed * JUMP_CONST;
+        chara->hurtbox->y = chara->y;
+  
     }
 
     if (trajectory == DOWN) {
@@ -196,8 +195,10 @@ void characterMove(character *chara, float steps, ushort trajectory, ushort max_
 void characterFlush(character *p1, character *p2, ushort max_x, ushort max_y) {
     if (p1->x <= p2->x) {
         characterMove(p1, 1.5, LEFT, max_x, max_y);
+        characterMove(p2, 1.5, RIGHT, max_x, max_y);
     } else {
         characterMove(p1, 1.5, RIGHT, max_x, max_y);
+        characterMove(p2, 1.5, LEFT, max_x, max_y);
     }
 };
 
